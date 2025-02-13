0d26b0561502e8349c9bcc38a4744c1c1b903c98
Tamara Abu Feilat <taabu-fe@c2r3s10.42amman.com>
2025-02-13 17:12:21 +0300

    push swap checks

--- libft/ft_realloc.c
+++ libft/ft_realloc.c
@@ -10,77 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
- /*#include "libft.h"
-
-void    *ft_realloc(void *ptr, size_t old_size, size_t new_size)
-{
-        void    *str;
-
-        str = malloc (new_size);
-        if (!str)
-        {
-                free (ptr);
-                return (NULL);
-        }
-        ft_memcpy(str, ptr, old_size);
-        free (ptr);
-        return (str);
-}*/
 #include "libft.h"
 
-/*void    *ft_realloc(void *ptr, size_t old_size, size_t new_size)
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
-    void    *str;
+	void	*str;
 
-    // إذا كان new_size == 0, يجب تحرير ptr وإرجاع NULL
-    if (new_size == 0)
-    {
-        free(ptr);
-        return NULL;
-    }
-
-    // إذا كان ptr == NULL, فقط قم بتخصيص new_size كما يفعل malloc
-    if (!ptr)
-        return malloc(new_size);
-
-    // تخصيص مساحة جديدة
-    str = malloc(new_size);
-    if (!str)
-        return NULL;  // لم نقم بتحرير ptr لأنه قد لا يزال يحتوي على بيانات مهمة
-
-    // نسخ البيانات بحجم الحد الأدنى بين old_size و new_size
-    ft_memcpy(str, ptr, (old_size < new_size) ? old_size : new_size);
-
-    // تحرير الذاكرة القديمة
-    free(ptr);
-
-    return str;
-}*/
-
-#include "libft.h"
-
-void    *ft_realloc(void *ptr, size_t old_size, size_t new_size)
-{
-    void    *new_ptr;
-
-    if (new_size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
-    if (!ptr)
-        return (malloc(new_size));
-
-    new_ptr = malloc(new_size);
-    if (!new_ptr)
-    {
-        free(ptr);
-        return (NULL);
-    }
-
-    ft_memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
-    free(ptr);
-    return (new_ptr);
+	str = malloc(new_size);
+	if (!str)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ft_memcpy(str, ptr, old_size);
+	free(ptr);
+	return (str);
 }
-
-
--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -18,7 +18,7 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	if(!s)
+	if (!s)
 		return (0);
 	while (s[i] != '\0')
 		i++;
